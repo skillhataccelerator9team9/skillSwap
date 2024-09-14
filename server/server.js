122b46ef777a48b170aebe864599b44777696f18
Ellie-Aghajani <z.aqajani@gmail.com>
2024-09-13 18:49:08 -0700

    changed message

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.json({ message: "Backend is running and connected!" });
+  res.json({ message: "Skill Swap" });
 });
 
 const PORT = process.env.PORT || 5000;
