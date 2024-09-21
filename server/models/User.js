b367814ff7624293dd2be4b0a969b55439bbe1f4
Ellie-Aghajani <z.aqajani@gmail.com>
2024-09-20 22:41:38 -0700

    update user model for point system

--- server/models/User.js
+++ server/models/User.js
@@ -1,24 +1,20 @@
-const mongoose = require("mongoose");
-
 const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  points: { type: Number, default: 100 },
+  profileImage: { type: String },
+  location: { type: String },
+  availableDays: { type: [String] },
+  skills: [
+    {
+      skillName: String,
+      levelOfExperience: String,
+      skillValue: Number,
+      skillImage: String,
+    },
+  ],
+  reviews: [{ reviewText: String, rating: Number }],
 });
 
 module.exports = mongoose.model("User", UserSchema);
