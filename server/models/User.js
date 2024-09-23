0b3f5680bb258bf505cb4efe50fcd39eff6a519b
Ellie-Aghajani <z.aqajani@gmail.com>
2024-09-22 17:08:12 -0700

    define collection nam

--- server/models/User.js
+++ server/models/User.js
@@ -1,14 +1,17 @@
 const mongoose = require("mongoose");
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  points: { type: Number, default: 100 },
-  profileImage: { type: String },
-  location: { type: String },
-  availableDays: { type: [String] },
-  skills: [{ type: mongoose.Schema.Types.ObjectId, ref: "Skill" }],
-  reviews: [{ reviewText: String, rating: Number }],
-});
+const UserSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    points: { type: Number, default: 100 },
+    profileImage: { type: String },
+    location: { type: String },
+    availableDays: { type: [String] },
+    skills: [{ type: mongoose.Schema.Types.ObjectId, ref: "Skill" }],
+    reviews: [{ reviewText: String, rating: Number }],
+  },
+  { collection: "user" }
+);
 
 module.exports = mongoose.model("User", UserSchema);
