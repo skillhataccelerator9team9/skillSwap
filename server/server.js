c3cf330fa5d3be0ec8842f7e93ebd6315b57f0e0
Ellie-Aghajani <z.aqajani@gmail.com>
2024-09-25 14:13:23 -0700

    remove the warnings related to useNewUrlParser and useUnifiedTopology.

--- server/server.js
+++ server/server.js
@@ -28,10 +28,7 @@ app.use(express.json());
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error: ", err.message));
 
