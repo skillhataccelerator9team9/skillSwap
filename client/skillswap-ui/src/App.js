6a4ce8f963ef61ecb9680ba391491089d1df4205
sandeepgill62 <sandeepgill62@gmail.com>
2024-09-26 20:11:29 -0500

    added screens home and contactUS

--- client/skillswap-ui/src/App.js
+++ client/skillswap-ui/src/App.js
@@ -1,14 +1,45 @@
-import React from "react";
-import "./App.css";
-import Introduction from "./components/Introduction";
+// import React from "react";
+// import "./App.css";
+// import Introduction from "./components/Introduction";
 
-function App () {
+// function App () {
+//   return (
+//     <div className="App">
+//       <Introduction />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react'
+import './App.css'
+
+import HomeScreen from './Screens/HomeScreen';
+import LoginScreen from './Screens/LoginScreen';
+import ContactScreen from './Screens/ContactScreen';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+const App = () => {
   return (
-    <div className="App">
-      <Introduction />
+    <div className="app">
+      <Router>
+        {
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/Home" element={<HomeScreen />} />
+            <Route path="/Login" element={<LoginScreen />} />
+            <Route path="/contact" element={<ContactScreen />} />
+          </Routes>
+        }
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
-
+export default App
