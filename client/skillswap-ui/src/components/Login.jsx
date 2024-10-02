3fdf15887948ba51a7fe5fa872a5bc0dd5a834da
sandeepgill62 <sandeepgill62@gmail.com>
2024-10-02 00:07:40 -0500

    update sign up with env variable

--- client/skillswap-ui/src/components/Login.jsx
+++ client/skillswap-ui/src/components/Login.jsx
@@ -3,6 +3,8 @@ import axios from "axios";
 
 import '../styles/login.css'
 
+const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
+
 const Login = () => {
 
   const [username, setUsername] = useState('');
@@ -11,6 +13,7 @@ const Login = () => {
   const [buttonName, setButtonName] = useState('Sign Up');
   const [message1, setMessage1] = useState('Already have an account?');
   const [message2, setMessage2] = useState('Sign In');
+  const [message3, setMessage3] = useState('successfully_signin');
 
   const switchLogin = (e) => {
     e.preventDefault();
@@ -37,7 +40,11 @@ const Login = () => {
         const body = JSON.stringify({ email, password });
         console.log(body);
 
-        const res = await axios.post("http://localhost:80/api/auth/login", body, config);
+        console.log({ apiBaseUrl });
+
+        const res = await axios.post(`${apiBaseUrl}/auth/login`, body, config);
+
+        //const res = await axios.post("http://localhost:80/api/auth/login", body, config);
 
         console.log(res.data); // This will contain the JWT token
         alert("Login successful!");
@@ -67,7 +74,11 @@ const Login = () => {
         const body = JSON.stringify({ username, email, password });
         console.log(body);
 
-        const res = await axios.post("http://localhost:80/api/auth/signup", body, config);
+        console.log({ apiBaseUrl });
+
+        const res = await axios.post(`${apiBaseUrl}/auth/signup`, body, config);
+
+        // const res = await axios.post("http://localhost:80/api/auth/signup", body, config);
 
         console.log(res.data); // This will contain the JWT token
         alert("Signup successful!");
