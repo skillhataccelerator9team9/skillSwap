b02e406b3256e7ee9495cc302ef455b9da493176
sandeepgill62 <sandeepgill62@gmail.com>
2024-10-06 03:01:04 -0500

    landing page nav bar

---
+++ client/skillswap-ui/src/screens/LandingPage.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+import LandingPageNavBar from '../components/LandingPageNavBar';
+
+function LandingPage () {
+  return (
+    <div className='LandingPage'>
+      <LandingPageNavBar />
+    </div>
+  )
+}
+
+export default LandingPage;
