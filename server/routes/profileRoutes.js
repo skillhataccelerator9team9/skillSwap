dee3cada842c5cb8f99a43b96d71d0facc569873
Ellie-Aghajani <z.aqajani@gmail.com>
2024-10-18 18:51:25 -0700

    modify profile routes to handle image uploads

--- server/routes/profileRoutes.js
+++ server/routes/profileRoutes.js
@@ -1,27 +1,36 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
+const upload = require("../config/uploadConfig");
 const User = require("../models/User");
 
 // @route   PUT /api/profile/update
 // @desc    Update profile of the authenticated user
 
-router.put("/update", authMiddleware, async (req, res) => {
-  const { profileImage, location, availableDays } = req.body;
+router.put(
+  "/update",
+  authMiddleware,
+  upload.single("profileImage"),
+  async (req, res) => {
+    const { location, availableDays } = req.body;
+    const profileImage = req.file ? req.file.path : null;
+    try {
+      const updatedData = { location, availableDays };
+      if (profileImage) {
+        updatedData.profileImage = profileImage;
+      }
 
-  try {
-    const user = await User.findByIdAndUpdate(
-      req.user.id,
-      { profileImage, location, availableDays },
-      { new: true }
-    );
+      const user = await User.findByIdAndUpdate(req.user.id, updatedData, {
+        new: true,
+      });
 
-    res.json(user);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server error");
+      res.json(user);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
   }
-});
+);
 
 // @route   GET /api/profile/userByEmail/:email
 // @desc    Get user information by email
